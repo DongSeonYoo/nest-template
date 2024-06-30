4ea1ca3a90b1f9a33cb5b0ee0b1e02909d5866b7
ehdtjs0612 <inko51366@naver.com>
2024-06-30 19:40:14 +0900

    feat: add prisma-exception filters

---
+++ src/filters/prisma-exception.filter.ts
@@ -0,0 +1,47 @@
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpStatus,
+  Logger,
+} from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { IExceptionResponse } from 'src/interfaces/response.interface';
+
+@Catch(Prisma.PrismaClientKnownRequestError)
+export class PrismaClientExceptionFilter implements ExceptionFilter {
+  constructor(private readonly logger: Logger) {}
+
+  catch(exception: Prisma.PrismaClientKnownRequestError, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const req = ctx.getRequest();
+    const res = ctx.getResponse();
+    const status: HttpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
+    let message: string;
+
+    switch (exception.code) {
+      case 'P2003':
+        message = 'Foreign key constraint failed';
+        break;
+
+      case 'P2002':
+        message = 'Unique constraint failed';
+        break;
+      // 여기에 하나씩 추가
+      default:
+        message = 'An unexpected error occurred';
+    }
+
+    // 세부사항 로그 & 응답은 500으로
+    this.logger.error(exception.message);
+
+    const response: IExceptionResponse = {
+      message: 'An unexpected error occurred',
+      requestURL: req.url,
+      statusCode: status,
+      timestamp: new Date(),
+    };
+
+    return res.status(HttpStatus.INTERNAL_SERVER_ERROR).send(response);
+  }
+}
