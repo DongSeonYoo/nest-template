8bafaf1b0cd29a6b254cf70d6c78ee1ad4359b6f
ehdtjs0612 <inko51366@naver.com>
2024-07-22 11:18:19 +0900

    refactor: use { Type } from @nestjs/common & add array response form

--- src/decorators/api-success.decorator.ts
+++ src/decorators/api-success.decorator.ts
@@ -1,29 +1,42 @@
-import { applyDecorators } from '@nestjs/common';
+import { applyDecorators, Type } from '@nestjs/common';
 import { ApiExtraModels, ApiOkResponse, getSchemaPath } from '@nestjs/swagger';
 
-export const ApiSuccess = <T>(schema: new (...args: any[]) => T) => {
+export const ApiSuccess = <T>(schema: Type<T> | [Type<T>]) => {
+  const isArray = Array.isArray(schema);
+  const actualSchema = isArray ? schema[0] : schema;
+
   return applyDecorators(
-    ApiExtraModels(schema),
+    ApiExtraModels(actualSchema),
     ApiOkResponse({
       schema: {
         properties: {
           statusCode: {
             type: 'number',
+            description: 'HTTP 상태 코드',
             example: 200,
           },
           message: {
             type: 'string',
+            description: '응답 메시지',
           },
           requestURL: {
             type: 'string',
+            description: '요청 URL',
           },
           timestamp: {
-            type: 'Date',
-            example: new Date(),
+            type: 'string',
+            format: 'date-time',
+            description: '응답 시간',
+            example: new Date().toISOString(),
           },
-          data: {
-            $ref: getSchemaPath(schema),
-          },
+          data: isArray
+            ? {
+                type: 'array',
+                items: { $ref: getSchemaPath(actualSchema) },
+              }
+            : {
+                $ref: getSchemaPath(actualSchema),
+              },
         },
       },
     }),
