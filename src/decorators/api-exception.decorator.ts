3f12995504505ee0e1ebe114cbe7e0127d91c5e6
ehdtjs0612 <inko51366@naver.com>
2024-10-11 16:36:52 +0900

    refactor: update api-exception-decorator

--- src/decorators/api-exception.decorator.ts
+++ src/decorators/api-exception.decorator.ts
@@ -1,35 +1,66 @@
 import { HttpException, Type, applyDecorators } from '@nestjs/common';
-import { ApiResponse } from '@nestjs/swagger';
+import { ApiExtraModels, ApiResponse, getSchemaPath } from '@nestjs/swagger';
+import { ExamplesObject } from '@nestjs/swagger/dist/interfaces/open-api-spec.interface';
+import { IExceptionResponse } from 'src/interfaces/response.interface';
 
-export const ApiException = (...errors: Array<Type<HttpException>>) => {
+export interface ErrorResponseOption {
+  /**
+   * 에러 예시의 제목
+   */
+  exampleTitle: string;
+
+  /**
+   * 에러 스키마
+   */
+  schema: Type<HttpException>;
+}
+
+export const ApiExceptions = (...errorResponses: ErrorResponseOption[]) => {
+  const examplesMap = new Map<number, ExamplesObject>();
+
+  errorResponses.forEach((err: ErrorResponseOption) => {
+    const errorInstance = new err.schema();
+    const statusCode = errorInstance.getStatus();
+    const errorResponse: IExceptionResponse = {
+      message: errorInstance.message,
+      requestURL: 'requestURL',
+      statusCode: statusCode,
+      timestamp: new Date(),
+    };
+
+    if (!examplesMap.has(statusCode)) {
+      examplesMap.set(statusCode, {});
+    }
+
+    const currentExamples = examplesMap.get(statusCode);
+    if (currentExamples) {
+      examplesMap.set(statusCode, {
+        ...currentExamples,
+        [err.exampleTitle]: {
+          value: errorResponse,
+        },
+      });
+    }
+  });
+
   return applyDecorators(
-    ...errors.map((error) => {
-      const errorInstance = new error();
-
-      return applyDecorators(
-        ApiResponse({
-          status: errorInstance.getStatus(),
-          schema: {
-            properties: {
-              statusCode: {
-                type: 'number',
-                example: errorInstance.getStatus(),
-              },
-              message: {
-                type: 'string',
-                example: errorInstance.message,
-              },
-              requestURL: {
-                type: 'string',
-              },
-              timestamp: {
-                type: 'Date',
-                example: new Date(),
-              },
+    ApiExtraModels(HttpException),
+    ...Array.from(examplesMap.entries()).map(([statusCode, examples]) =>
+      ApiResponse({
+        status: statusCode,
+        content: {
+          'application/json': {
+            schema: {
+              oneOf: [
+                {
+                  $ref: getSchemaPath(HttpException),
+                },
+              ],
             },
+            examples: examples,
           },
-        }),
-      );
-    }),
+        },
+      }),
+    ),
   );
 };
--- src/decorators/jwt-auth.decorator.ts
+++ src/decorators/jwt-auth.decorator.ts
@@ -1,6 +1,6 @@
 import { UseGuards, applyDecorators } from '@nestjs/common';
 import { ApiBearerAuth } from '@nestjs/swagger';
-import { ApiException } from './api-exception.decorator';
+import { ApiExceptions } from './api-exception.decorator';
 import { JwtGuard } from 'src/apis/auth/guards/jwt.guard';
 import { JwtAuthException } from 'src/apis/auth/exceptions/jwt-auth-exception';
 
@@ -8,6 +8,9 @@ export const LoginAuth = () => {
   return applyDecorators(
     ApiBearerAuth(),
     UseGuards(JwtGuard),
-    ApiException(JwtAuthException),
+    ApiExceptions({
+      exampleTitle: '로그인 하지 않았을 경우',
+      schema: JwtAuthException,
+    }),
   );
 };
