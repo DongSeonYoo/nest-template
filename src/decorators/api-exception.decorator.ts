9483df34dc0fc533b4b18da3b4a84f6561bd37f6
ehdtjs0612 <inko51366@naver.com>
2024-10-11 16:33:17 +0900

    refactor: modify api-exception-swagger-decorator

--- src/decorators/api-exception.decorator.ts
+++ src/decorators/api-exception.decorator.ts
@@ -1,29 +1,35 @@
-import { HttpStatus, applyDecorators } from '@nestjs/common';
+import { HttpException, Type, applyDecorators } from '@nestjs/common';
 import { ApiResponse } from '@nestjs/swagger';
 
-export const ApiException = (status: HttpStatus, description: string) => {
+export const ApiException = (...errors: Array<Type<HttpException>>) => {
   return applyDecorators(
-    ApiResponse({
-      status: status,
-      schema: {
-        properties: {
-          statusCode: {
-            type: 'number',
-            example: status,
+    ...errors.map((error) => {
+      const errorInstance = new error();
+
+      return applyDecorators(
+        ApiResponse({
+          status: errorInstance.getStatus(),
+          schema: {
+            properties: {
+              statusCode: {
+                type: 'number',
+                example: errorInstance.getStatus(),
+              },
+              message: {
+                type: 'string',
+                example: errorInstance.message,
+              },
+              requestURL: {
+                type: 'string',
+              },
+              timestamp: {
+                type: 'Date',
+                example: new Date(),
+              },
+            },
           },
-          message: {
-            type: 'string',
-            example: description,
-          },
-          requestURL: {
-            type: 'string',
-          },
-          timestamp: {
-            type: 'Date',
-            example: new Date(),
-          },
-        },
-      },
+        }),
+      );
     }),
   );
 };
--- src/decorators/jwt-auth.decorator.ts
+++ src/decorators/jwt-auth.decorator.ts
@@ -1,12 +1,13 @@
-import { HttpStatus, UseGuards, applyDecorators } from '@nestjs/common';
+import { UseGuards, applyDecorators } from '@nestjs/common';
 import { ApiBearerAuth } from '@nestjs/swagger';
 import { ApiException } from './api-exception.decorator';
 import { JwtGuard } from 'src/apis/auth/guards/jwt.guard';
+import { JwtAuthException } from 'src/apis/auth/exceptions/jwt-auth-exception';
 
 export const LoginAuth = () => {
   return applyDecorators(
     ApiBearerAuth(),
     UseGuards(JwtGuard),
-    ApiException(HttpStatus.UNAUTHORIZED, '로그인 필요한 서비스입니다'),
+    ApiException(JwtAuthException),
   );
 };
